6a0af32c7391be5ebda2e23bd68df8c649305fb7
Nathan Pickard <nathan.pickard87@gmail.com>
2016-05-16 18:13:55 -0700

    Initial commit

---
+++ src/com/teamtreehouse/pomodoro/model/Attempt.java
@@ -0,0 +1,32 @@
+package com.teamtreehouse.pomodoro.model;
+
+/**
+ * Created by Nathan Pickard on 5/16/2016.
+ */
+public class Attempt {
+    private String mMessage;
+    private int mRemainingSeconds;
+    private AttemptKind mKind;
+
+    public Attempt(AttemptKind kind, String message) {
+        mKind = kind;
+        mMessage = message;
+        mRemainingSeconds = kind.getTotalSeconds();
+    }
+
+    public String getMessage() {
+        return mMessage;
+    }
+
+    public int getRemainingSeconds() {
+        return mRemainingSeconds;
+    }
+
+    public AttemptKind getKind() {
+        return mKind;
+    }
+
+    public void setMessage(String message) {
+        mMessage = message;
+    }
+}
